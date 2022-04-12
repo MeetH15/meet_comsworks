45bc2713949a3655ddefe7d31db246e4758c0ef5
meet <meethadiyal739@gmail.com>
2022-04-12 16:51:48 +0530

    Updates

---
+++ precache-manifest.ee864d439b4ca53b65ac0648448bcd48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45ed97c9cc06c5bf9f2b",
+    "url": "/meet_comsworks/static/css/main.e3355a92.chunk.css"
+  },
+  {
+    "revision": "45ed97c9cc06c5bf9f2b",
+    "url": "/meet_comsworks/static/js/main.42d895ff.chunk.js"
+  },
+  {
+    "revision": "4b89aded3b15340e2b85",
+    "url": "/meet_comsworks/static/js/runtime~main.64be6cb4.js"
+  },
+  {
+    "revision": "63a58ebecc939e70e642",
+    "url": "/meet_comsworks/static/js/2.78d5d366.chunk.js"
+  },
+  {
+    "revision": "9426d59aa7ab270cecc19f17db99984c",
+    "url": "/meet_comsworks/static/media/newLogo.9426d59a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/meet_comsworks/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "6e8d476d7e73748a3538df5437a4e3db",
+    "url": "/meet_comsworks/index.html"
+  }
+];
